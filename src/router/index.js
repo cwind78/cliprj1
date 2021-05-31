baa18e27c2baf12d7365842377c95959cbd17a3b
kwangsu <lotus78@nate.com>
2021-06-01 00:39:20 +0900

    first commit and push

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Notfound from '../views/404.vue'
+import Pratice from '../views/Pratice.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/pratice',
+    name: 'Pratice',
+    component: Pratice
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '*',
+    name: 'Notfound',
+    component: Notfound
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/Pratice.vue
@@ -0,0 +1,96 @@
+<template>
+	<div>
+		<h1>Pratice</h1>
+		<div>
+			<table>
+				<tr v-for="n in 5" v-bind:key="n">
+					<td @click="reserve(n, m, $event)" v-for="m in 5" v-bind:key="m"></td>
+				</tr>
+			</table>
+		</div>
+		<div>
+			<button v-if="check" @click="checkArray()">Check</button>
+			<button @click="initArray()">Retry</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Pratice",
+	data: function() {
+		return {
+			tr: [],
+			td: [],
+			check: false
+		}
+	},
+	methods: {
+		reserve : function(n, m, event) {
+			// console.log('n='+n+', m='+m+', this.tr.length='+this.tr.length+', this.td.length='+this.td.length)
+			// console.log(event.target.classList.contains('td-color'))
+			
+			if (event.target.classList.contains('td-color')) {
+				event.target.classList.remove('td-color')
+				console.log(this.td[this.tr.indexOf(n)].length)
+				if (this.td[this.tr.indexOf(n)].length <= 1) {
+					this.td.splice(this.tr.indexOf(n), 1)
+					this.tr.splice(this.tr.indexOf(n), 1)
+				} else {
+					this.td[this.tr.indexOf(n)].splice(this.td[this.tr.indexOf(n)].indexOf(m), 1)
+				}
+			} else {
+				event.target.classList.add('td-color')
+
+				if (this.tr.length === 0) {
+					this.tr.push(n)
+					this.td.push([m])
+				} else {
+					if (this.tr.indexOf(n) === -1) {
+						this.tr.push(n)
+						this.td.push([m])
+					} else {
+						if (this.td[this.tr.indexOf(n)].indexOf(m) === -1) {
+							this.td[this.tr.indexOf(n)].push(m)
+						}
+					}
+				}
+			}
+		},
+		checkArray: function() {
+			console.log(this.tr.toString());
+			console.log(this.td.toString());
+		},
+		initArray: function() {
+			this.tr = [];
+			this.td = [];
+
+			var tds = document.querySelectorAll(".td-color")
+			// console.log(tds.length)
+			for (var i=0; i<tds.length; i++) {
+				tds[i].classList.remove("td-color")
+			}
+		}
+	}
+
+}
+</script>
+<style>
+td {
+	border: 1px solid black;
+	width: 30px;
+	height: 30px;
+}
+
+.td-color {
+	background-color: yellow;
+}
+
+div {
+	border: 1px solid red;
+}
+
+table {
+	margin: auto;
+}
+</style>
